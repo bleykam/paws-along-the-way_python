69b9b557136d49b4263f9f2d19b875213576cefd
bleykam <bleykam@gmail.com>
2023-10-01 15:59:47 -0400

    updated edit animal

--- paws_site/paws_client/src/components/EditAnimal/EditAnimal.jsx
+++ paws_site/paws_client/src/components/EditAnimal/EditAnimal.jsx
@@ -1,29 +1,28 @@
 import "./EditAnimal.scss"
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate} from "react-router-dom";
 import { useState, useEffect} from 'react';
 import axios from 'axios';
 import { useGetEffect, base_url } from "../../utils";
 
 export default function EditAnimal() {
+    const navigate = useNavigate();
     const { animalId } = useParams();
 	const [orgList, setOrgList] =useState("");
     const[animal, setAnimal]=useState("");
-   
+
     useGetEffect(`${base_url}/api/organizations/`, setOrgList);
     useGetEffect(`${base_url}/api/animals/${animalId}`, setAnimal);
   
-
    const [values, setValues] = useState({
-    description: "",  name: "", gender: "",  type: "", species: "", breeds: "", colors: "", age: "", coat: "", active: "", organization: "",
+    description: "",  name: "", gender: "", type: "", species: "", breeds: "", colors: "", age: "", coat: "", active: "", organization: "",
     });
     
-    const [checked, setChecked]=useState({
+    const [checkbox, setCheckbox]=useState({
         dogs: "", cats: "", children: "", spayed_neutered: "", house_trained: "", declawed: "", special_needs: "", shots_current: "", mixed: "", 
         });
-    const [changes, setChanges] = useState({})
-    const updateValues = 
-    
-    (animal) => {
+
+
+    const updateValues = (animal) => {
         if (animal) {
             setValues(prevValues => ({
                 ...prevValues,
@@ -40,7 +39,7 @@ export default function EditAnimal() {
                 active: animal.active,
                 organization: animal.organization
             }));
-            setChecked(prevValues => ({
+            setCheckbox(prevValues => ({
                 ...prevValues,
                 dogs: animal.environment.dogs,
                 cats: animal.environment.cats,
@@ -59,67 +58,70 @@ export default function EditAnimal() {
         updateValues(animal);
     }, [animal]);
   
-    console.log("VALUES",values); 
-    console.log("CHECKED",checked); 
-    let AObj ={
-        "attributtes":{
-        "spayed_neutered": checked.spayed_neutered,
-        "house_trained": checked.house_trained,
-        "declawed": checked.declawed,
-        "special_needs": checked.special_needs,
-        "shots_current": checked.shots_current,
-        "mixed": checked.mixed,
-
-        },
-        values,
-    }
-    console.log("AOBJ", AObj)
-    
- 
-    
 
     const handleChange = (event) => {
         if (!event) {
             return;
         }
-
+       
         const { name, value } = event.target;
-            
-            setValues({ ...values, [name]: value });
-            setChanges({ ...changes, [name]: value });
-            
-        console.log("CHANGE VAL", changes)
+        setValues({ ...values, [name]: value });
+    };
+ 
+    
+    const handleCheckChange = (event) => {
+        if (!event) {
+            return;
+        }
+
+        const { name, checked } = event.target;
+        setCheckbox({ ...checkbox, [name]: checked });
     };
 
     const handleSubmit = (event) => {
         event.preventDefault();
-       
-        // for (const field in EditAnimal) {
-        //     if (EditAnimal[field] !== animal[field] && EditAnimal[field] !== "" ) {
-        //     editReq[field] = EditAnimal[field];
-        //   }
-        // }
-   
-        console.log("submit changes", changes)
-        axios.put(`${base_url}/api/animals/${animalId}/`, changes)
-            .then((response) => {
-                console.log("response", response)
-                event.target.reset();
+
+        let EditAnimal ={
+            attributes:{
+                spayed_neutered: checkbox.spayed_neutered,
+                house_trained: checkbox.house_trained,
+                declawed: checkbox.declawed,
+                special_needs: checkbox.special_needs,
+                shots_current: checkbox.shots_current,
+                mixed: checkbox.mixed,
+            },
+            environment: {
+                children:checkbox.children,
+                dogs: checkbox.dogs,
+                cats: checkbox.cats
+            },
+            description: values.description,
+            name: values.name,
+            gender: values.gender,
+            size: values.size,
+            type: values.type,
+            species: values.species,
+            breeds: values.breeds,
+            colors: values.colors,
+            age: values.age,
+            coat: values.coat,
+            active: values.active,
+            organization: values.organization
+        }
+        
+        axios.put(`${base_url}/api/animals/${animalId}/`, EditAnimal)
+            .then(() => {
+                navigate(`/animal/${animalId}`);
             })
             .catch((error) => {
+                console.error(error);
             });
-
-        // for (let key in EditAnimal) {
-        //     EditAnimal[key] = "";
-        // }
-    
     }
         
-
-
     return (
+     
         <main className="add-animal">
-    {/* <form method="put" onSubmit={handleSubmit}> */}
+
             <div className="create-request__div">
                 <label className="create-request__label" htmlFor="name">Animal Name: </label>
                 <input className="create-request__input" id="name" name="name" value={values.name} onChange={handleChange} />
@@ -137,27 +139,27 @@ export default function EditAnimal() {
 
             <div className="create-request__div">
                 <label className="create-request__label" htmlFor="type">Type: </label>
-                <input className="create-request__input" id="type" name="type" onChange={handleChange}  defaultValue={values.type} />
+                <input className="create-request__input" id="type" name="type" onChange={handleChange}  value={values.type} />
             </div>
 
             <div className="create-request__div">
                 <label className="create-request__label" htmlFor="species">Species: </label>
-                <input className="create-request__input" id="species" name="species" onChange={handleChange} defaultValue={values.species} />
+                <input className="create-request__input" id="species" name="species" onChange={handleChange} value={values.species} />
             </div>
 
             <div className="create-request__div">
                 <label className="create-request__label" htmlFor="breeds">Breeds: </label>
-                <input className="create-request__input" id="breeds" name="breeds" onChange={handleChange}  defaultValue={values.breeds}/>
+                <input className="create-request__input" id="breeds" name="breeds" onChange={handleChange}  value={values.breeds}/>
             </div>
 
             <div className="create-request__div">
                 <label className="create-request__label" htmlFor="colors">Colors: </label>
-                <input className="create-request__input" id="colors" name="colors" onChange={handleChange} defaultValue={values.colors}  />
+                <input className="create-request__input" id="colors" name="colors" onChange={handleChange} value={values.colors}  />
             </div>
 
             <div className="create-request__div">
                 <label className="create-request__label" htmlFor="coat">Coat: </label>
-                <input className="create-request__input" id="coat" name="coat" onChange={handleChange} defaultValue={values.coat} />
+                <input className="create-request__input" id="coat" name="coat" onChange={handleChange} value={values.coat} />
             </div>
 
 
@@ -196,30 +198,30 @@ export default function EditAnimal() {
 
             <div className="create-request__div">
                 <label className="create-request__label" htmlFor="photo">Photo: </label>
-                <input className="create-request__input" id="photo" name="photo" onChange={handleChange} defaultValue={values.photos}  />
+                <input className="create-request__input" id="photo" name="photo" onChange={handleChange} value={values.photos}  />
             </div>
 
 
             <div className="create-request__div">
                 <label className="create-request__label" htmlFor="description">Description:  </label>
-                <textarea className='create-request__input' defaultValue={values.description} type="text" name="description" id="descritpion" placeholder='Enter Request details' onChange={handleChange} />
+                <textarea className='create-request__input' value={values.description} type="text" name="description" id="descritpion" placeholder='Enter Request details' onChange={handleChange} />
             </div>
 
             <div className='add-animal__env'>
                 <h4 className="add-animal__title">Friendly With:</h4>
 
                 <div className="checkbox-wrapper-13 add-animal__box">
-                    <input id="c1-13" type="checkbox" name="children" checked={values.children ?? ''} onChange={handleChange} />
+                    <input id="c1-13" type="checkbox" name="children" checked={checkbox.children} onChange={handleCheckChange} />
                     <label htmlFor="c1-13">Children</label>
                 </div>
 
                 <div className="checkbox-wrapper-13 add-animal__box">
-                    <input id="c1-13" type="checkbox" name="dogs" checked={values.dogs ?? ''} onChange={handleChange} />
+                    <input id="c1-13" type="checkbox" name="dogs"  checked={checkbox.dogs}  onChange={handleCheckChange} />
                     <label htmlFor="c1-13">Dogs</label>
                 </div>
 
                 <div className="checkbox-wrapper-13 add-animal__box">
-                    <input id="c1-13" type="checkbox" name="cats" checked={values.cats ?? ''} onChange={handleChange} /> 
+                    <input id="c1-13" type="checkbox" name="cats" checked={checkbox.cats} onChange={handleCheckChange} /> 
                     <label htmlFor="c1-13">Cats</label>
                 </div>
 
@@ -228,32 +230,32 @@ export default function EditAnimal() {
             <div className='add-animal__env'>
 
                 <div className="checkbox-wrapper-13 add-animal__box">
-                    <input id="c1-13" type="checkbox" name="spayed_neutered" checked={values.spayed_neutered ?? ''} onChange={handleChange} />
-                    <label htmlFor="c1-13">Spayed/Nuetered</label>
+                    <input id="c1-13" type="checkbox" name="spayed_neutered" checked={checkbox.spayed_neutered} onChange={handleCheckChange} />
+                    <label htmlFor="c1-13">Spayed/Neutered</label>
                 </div>
 
                 <div className="checkbox-wrapper-13 add-animal__box">
-                    <input id="c1-13" type="checkbox" name="house_trained" checked={values.house_trained ?? ''} onChange={handleChange} />
+                    <input id="c1-13" type="checkbox" name="house_trained" checked={checkbox.house_trained} onChange={handleCheckChange} />
                     <label htmlFor="c1-13">House Trained</label>
                 </div>
 
                 <div className="checkbox-wrapper-13 add-animal__box">
-                    <input id="c1-13" type="checkbox" name="declawed" checked={values.declawed ?? ''} onChange={handleChange} />
+                    <input id="c1-13" type="checkbox" name="declawed" checked={checkbox.declawed ?? ''} onChange={handleCheckChange} />
                     <label htmlFor="c1-13">Declawed</label>
                 </div>
 
                 <div className="checkbox-wrapper-13 add-animal__box">
-                    <input id="c1-13" type="checkbox" name="special_needs" checked={values.special_needs ?? ''} onChange={handleChange} />
+                    <input id="c1-13" type="checkbox" name="special_needs" checked={checkbox.special_needs} onChange={handleCheckChange} />
                     <label htmlFor="c1-13">Special Needs</label>
                 </div>
 
                 <div className="checkbox-wrapper-13 add-animal__box">
-                    <input id="c1-13" type="checkbox" name="shots_current" checked={values.shots_current ?? ''} onChange={handleChange} />
+                    <input id="c1-13" type="checkbox" name="shots_current" checked={checkbox.shots_current} onChange={handleCheckChange} />
                     <label htmlFor="c1-13">Shots Current</label>
                 </div>
 
                 <div className="checkbox-wrapper-13 add-animal__box">
-                    <input id="c1-13" type="checkbox" name="mixed" checked={values.mixed ?? ''} onChange={handleChange} />
+                    <input id="c1-13" type="checkbox" name="mixed" checked={checkbox.mixed } onChange={handleCheckChange} />
                     <label htmlFor="c1-13">Mixed</label>
                 </div>
             </div>
@@ -263,6 +265,7 @@ export default function EditAnimal() {
             </div>
 
         </main>
+   
     )
 
 
